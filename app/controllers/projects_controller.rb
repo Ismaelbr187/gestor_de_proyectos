819dddcb2f826ce6ba71ab0faa35d4c880726384
Ismael <ismael.br187@gmail.com>
2020-08-16 23:32:11 -0400

    basic authenticate

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,6 @@
 class ProjectsController < ApplicationController
+  http_basic_authenticate_with name: 'desafiovamoscontodo', password: 'XAHTJEAS23123%23', except: [:dashboard]
+
   def dashboard
     @projects = Project.all
   end
@@ -8,5 +10,17 @@ class ProjectsController < ApplicationController
   end
 
   def create
+    @project = Project.create(project_params)
+    if @project.valid?
+      @project.save
+      redirect_to root_path, notice: "Se guardó el proyecto correctamente"
+    else 
+      redirect_to root_path, notice: "No se pudo guardar el proyecto"
+    end
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(:name, :description, :begin, :end, :state)
   end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,9 @@
 class Project < ApplicationRecord
+    validates_presence_of :name, :description, :begin, :end, :state
+   
+    enum state: [ "propuesta", "en progreso", "terminado" ]
     #  enum status: [ :active, :archived ]
     # 0: propuesta
     # 1: en progreso
     # 2: terminado
-
-    enum state: [ "propuesta", "en progreso", "terminado" ]
 end
